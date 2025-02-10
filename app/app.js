dbd4d5298cf32bc0d957505dfba7f18a3c951ec5
benie-joy-possi <beniejoypossi@gmail.com>
2025-02-10 12:04:38 +0100

    added app.js

--- app/app.js
+++ app/app.js
@@ -15,3 +15,26 @@ function App() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import TechPrograms from "./components/TechPrograms";
+import Testimonials from "./components/Testimonials";
+import CompanyFunding from "./components/CompanyFunding";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />           {/* Navigation bar */}
+      <Hero />             {/* Hero section */}
+      <TechPrograms />     {/* Programs section */}
+      <CompanyFunding />   {/* Funding section */}
+      <Testimonials />     {/* Testimonials section */}
+      <Footer />           {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
