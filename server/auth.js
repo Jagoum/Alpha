e8980e6c16364cf50f19957510e94c7ead163abb
anonym-266 <desmondtardzenyuy@gmail.com>
2025-02-10 11:11:33 +0100

    fix: installed express session and included package.json for server

--- server/auth.js
+++ server/auth.js
@@ -1,3 +1,28 @@
+// const passport = require("passport");
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// require('dotenv').config();
+
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: "/auth/google/callback",
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       return done(null, profile);
+//     }
+//   )
+// );
+
+// module.exports = passport;
+
+
+require('dotenv').config();
+
+console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
+
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
